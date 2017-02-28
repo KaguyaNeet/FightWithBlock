494ed0e034ee13398c29aa48cb379a18a6516cae
PanXiaoyi <552448191@qq.com>
2017-02-28 22:08:15 +0800

    空项目
    空项目

---
+++ FightWithBlock/Intermediate/Build/Win64/UE4Editor/Development/FightWithBlock/PCH.FightWithBlock.cpp
@@ -0,0 +1,1 @@
+#include "E:/UnrealProjects/FightWithBlock/Intermediate/Build/Win64/UE4Editor/Development/FightWithBlock/PCH.FightWithBlock.h"
---
+++ FightWithBlock/Source/FightWithBlock/FightWithBlockGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "FightWithBlockGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIGHTWITHBLOCK_API AFightWithBlockGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
